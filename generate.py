c0966ecdacc3697a4b8e31d37e5059199c68e864
Oleksii <theburninhell@gmail.com>
2020-06-09 12:57:28 +0300

    init

---
+++ generate.py
@@ -0,0 +1,43 @@
+# sudo pip install --user websocket-client
+from websocket import create_connection
+import sys
+from json import loads
+from subprocess import call
+from datetime import datetime
+import os
+
+
+class mailbox(object):
+    """10 minute mailbox"""
+
+    def __init__(self):
+        super(mailbox, self).__init__()
+        self.ws = create_connection("wss://dropmail.me/websocket")
+        self.next = self.ws.recv
+        self.close = self.ws.close
+        self.email = self.next()[1:].split(":")[0]
+        self.next()
+
+
+def main(box):
+    # stdlib
+    call(["echo '{0}' | pbcopy".format(box.email)], shell=True)
+    print(box.email + " was copied to clipboard")
+    while True:
+        result = box.next()
+        try:
+            print("Recieved following at {0}".format(datetime.now()))
+            for k in loads(result[1:]).items():
+                print("\t%s: %s" % k)
+        except:
+            print("Recieved:{1} {0}\n".format(result, datetime.now()))
+
+
+if __name__ == '__main__':
+    print("PID: {0}\nIf you can't quite, run 'kill {0}'\n".format(os.getpid()))
+    try:
+        box = mailbox()
+        main(box)
+    except KeyboardInterrupt:
+        box.close()
+        sys.exit(0)
